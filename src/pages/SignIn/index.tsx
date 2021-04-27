a8d0de16d54366d802c2308e5e372daf88007ba7
nomorehorizon <lmachado72@outlook.com>
2021-04-27 18:46:14 -0300

    login screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { Image } from "react-native";
 
-import { Container } from "./styles";
+import { Container, Title } from "./styles";
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+import logoImg from "../../assets/logo.png";
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log("Cringe");
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
 };
 
 export default SignIn;
