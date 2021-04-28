2cc7db826ee5c81fb77e44f4f95780f12abfe95e
nomorehorizon <lmachado72@outlook.com>
2021-04-28 16:10:01 -0300

    finish

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -6,11 +6,12 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 30 : 40}px;
+  /* align-content: center; */
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
 export const Title = styled.Text`
-  font-size: 24px;
+  font-size: 20px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
@@ -21,9 +22,9 @@ export const ForgotPassword = styled.TouchableOpacity`
 `;
 
 export const ForgotPasswordText = styled.Text`
-  color: #f4ede8;
   font-size: 16px;
-  font-family: 'RobotoSlab-Regular';
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
 `;
 
 export const CreateAccountButton = styled.TouchableOpacity`
@@ -31,18 +32,18 @@ export const CreateAccountButton = styled.TouchableOpacity`
   left: 0;
   bottom: 0;
   right: 0;
-  background: #312e38;
+  background-color: #312e38;
   border-top-width: 1px;
   border-color: #232129;
   padding: 16px 0 ${16 + getBottomSpace()}px;
-
   justify-content: center;
   align-items: center;
   flex-direction: row;
 `;
 
-export const CreateAccountButtonText = styled.Text`
-  color: #ff9000;
+export const CreateAccountText = styled.Text`
   font-size: 18px;
-  font-family: 'RobotoSlab-Regular';
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+  margin-left: 16px;
 `;
